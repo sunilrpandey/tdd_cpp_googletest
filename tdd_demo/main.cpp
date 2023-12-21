93c60292196d0c35287039cd513989087093a10f
Sunil Pandey <inkingmind@gmail.com>
2023-12-21 13:44:37 +0530

    gtest lib link and TEST_F demo

--- tdd_demo/main.cpp
+++ tdd_demo/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include <gtest/gtest.h>
 
 using namespace std;
 
+
+class SampleClass
+{
+    public:
+    int sampleFunc()
+    {
+        return 1;
+    }
+};
 
 int Factorial(int n) {
     if (n == 0 || n == 1)
@@ -10,17 +20,48 @@ int Factorial(int n) {
         return n * Factorial(n - 1);
 }
 
- TEST(FactorialTest, HandleZeroInput) {
- 	EXPECT_EQ(Factorial(0), 1);
- }
-// #define Init_done_in_CMake 1
-// #if Init_done_in_CMake
+//Simple Test
+TEST(FactorialTest, HandleZeroInput) {
+    EXPECT_EQ(Factorial(0), 1);
+}
+
+// TestFixture
+class TestFixtureDemo : public testing::Test
+{
+    public:
+    TestFixtureDemo(){
+        cout << "\nConstructed" << endl;
+    }
+    ~TestFixtureDemo(){
+        cout << "\nDestructed" << endl;
+    }
+    protected:
+    void SetUp() override
+    {
+        cout << "\nSetUp" << endl;
+        pEx = new SampleClass();
+    }
+    void TearDown()override
+    {
+        cout << "\nTearDown" << endl;
+        delete pEx;
+    }
+
+    SampleClass * pEx;
+};
+TEST_F(TestFixtureDemo, test1){
+    EXPECT_EQ(1,pEx->sampleFunc());
+}
+TEST_F(TestFixtureDemo, test2){
+    EXPECT_NE(0,pEx->sampleFunc());
+}
+
+#ifndef GTEST_MAIN_INIT_IN_CMAKE
 int main(int argc, char* argv[])
 {
-
-    cout << "populating tests from gtest main" << endl;
+    cout << "\n\n\n\n*****\n\nPopulating tests from gtest main\n\n\n\n" << endl;
 
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
-//#endif 
+#endif 
